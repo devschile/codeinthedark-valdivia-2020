7703c07899676382bca8fe6e9c422d8723f8c1f2
Raul Hernandez <raulghm@gmail.com>
2020-01-04 00:17:03 -0300

    fix: mejoras varias

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
   // Set local elements
   const doc = document.documentElement
   const button = document.querySelector('.Hero .Button')
+  const videoPlayerBack = document.querySelector('.Video-backdrop')
   const hero = document.querySelector('.Hero')
 
   // Remove no-js class
@@ -17,6 +18,22 @@
     hero.classList.remove('is-over')
   })
 
+  // Replace video player
+  videoPlayerBack.addEventListener('click', () => {
+    console.log('click')
+    var options = {
+      id: 63318482,
+      width: 900,
+      height: 506,
+      title: false,
+      byline: false,
+      portrait: false
+    }
+
+    const player = new Vimeo.Player('video', options)
+    setTimeout(() => player.play(), 200)
+  })
+
   // Reveal animations
   if (document.body.classList.contains('has-animations')) {
     // eslint-disable-next-line no-undef
@@ -27,21 +44,21 @@
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       origin: 'top',
       interval: 100,
-      delay: 100
+      delay: 200
     }
 
     sr.reveal('.Hero', slide)
-    sr.reveal('.Hero-image', { ...slide, delay: 500, scale: 1.1 })
-    sr.reveal('.Hero-message', { ...slide, delay: 600 })
+    sr.reveal('.Hero-image', { ...slide, delay: 800, scale: 1.2 })
+    sr.reveal('.Hero-message', { ...slide, delay: 1000 })
     sr.reveal('.Hero-ticket', {
       ...slide,
-      delay: 1000,
+      delay: 1200,
       duration: 2000
     })
+    sr.reveal('.Video-player', slide)
     sr.reveal('.Video Label', slide)
-    sr.reveal('.Video iframe', slide)
     sr.reveal('.Footer', { ...slide, origin: 'bottom' })
-    sr.reveal('.Socialbar', { ...slide, origin: 'bottom', delay: 2000 })
+    sr.reveal('.Socialbar', { ...slide, delay: 2000 })
 
     const gridNodeList = document.querySelectorAll('.flex .item')
     sr.reveal(gridNodeList, { ...slide, origin: 'bottom' })
